0e74f3d7117037d14df7f94d4302f5b3657e5662
jaeeunhyun <rosa2070@naver.com>
2021-04-06 15:59:55 +0900

    xgdsgd

---
+++ src/main/java/hello/core1/AppConfig.java
@@ -0,0 +1,33 @@
+package hello.core1;
+
+import hello.core1.discount.DiscountPolicy;
+import hello.core1.discount.FixDiscountPolicy;
+import hello.core1.discount.RateDiscountPolicy;
+import hello.core1.member.MemberRepository;
+import hello.core1.member.MemberService;
+import hello.core1.member.MemberServiceImpl;
+import hello.core1.member.MemoryMemberRepository;
+import hello.core1.order.OrderService;
+import hello.core1.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+
+}
--- src/test/java/hello/core1/order/OrderServiceTest.java
+++ src/test/java/hello/core1/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package hello.core1.order;
 
+import hello.core1.AppConfig;
 import hello.core1.member.Grade;
 import hello.core1.member.Member;
 import hello.core1.member.MemberService;
 import hello.core1.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
