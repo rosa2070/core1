9d1ec98044cc9294a28d40e072a4f95dbe4d3da5
jaeeunhyun <rosa2070@naver.com>
2021-04-07 15:58:17 +0900

    ddddddddd

--- src/main/java/hello/core1/AppConfig.java
+++ src/main/java/hello/core1/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core1.member.MemberServiceImpl;
 import hello.core1.member.MemoryMemberRepository;
 import hello.core1.order.OrderService;
 import hello.core1.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
