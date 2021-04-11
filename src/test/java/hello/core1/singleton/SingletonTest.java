2309b9c55275d998f9a6dea74e0e96cd5a19e67f
jaeeunhyun <rosa2070@naver.com>
2021-04-11 23:08:52 +0900

    dddddd

---
+++ src/test/java/hello/core1/singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package hello.core1.singleton;
+
+import hello.core1.AppConfig;
+import hello.core1.member.Member;
+import hello.core1.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void putContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+    }
+}
