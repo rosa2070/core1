1025071b688f3a058b8633a9b8dee6d0cbf2c651
jaeeunhyun <rosa2070@naver.com>
2021-04-13 16:39:55 +0900

    lmlkm

--- src/main/java/hello/core1/AutoAppConfig.java
+++ src/main/java/hello/core1/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core1;
 
+import hello.core1.member.MemberRepository;
+import hello.core1.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
